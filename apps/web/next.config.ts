8b2f218de7ad11809f9c5ade2bc9b15251796552
Anthony Bechay <anthonybechay1@gmail.com>
2026-03-19 15:27:58 +0200

    ad

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,9 +2,16 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "standalone",
   outputFileTracingRoot: path.join(process.cwd(), "../../"),
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-1f6ef2bce8ec46fa9b8fd340b1b671fd.r2.dev",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
